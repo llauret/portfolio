ea201c1f851e7732ec4fda97da79dbcba79e6f95
lauret lucas <lucas.lauret@edu.univ-fcomte.fr>
2024-12-16 18:57:53 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/llauret-iut90.github.io/",
+  base: "/llauret/",
   plugins: [
     vue(),
     vueDevTools(),
